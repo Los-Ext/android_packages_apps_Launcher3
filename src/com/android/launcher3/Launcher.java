556b2046a58d9b53ecd6ce604b383534f83d65e5
ImSpiDy <SpiDy2713@gmail.com>
2025-05-27 19:28:39 +0000

    Revert "Launcher3: clean heap memory when leaving launcher"
    This reverts commit e1bb1ec645df81219f375deac46fa753beb99335.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -338,9 +338,6 @@ public class Launcher extends StatefulActivity<LauncherState>
     private final KeyboardShortcutsDelegate mKeyboardShortcutsDelegate =
             new KeyboardShortcutsDelegate(this);
 
-    private static final long GC_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
-    private long lastGcTime = 0L;
-
     @Thunk
     Workspace<?> mWorkspace;
     @Thunk
@@ -1322,20 +1319,6 @@ public class Launcher extends StatefulActivity<LauncherState>
         return Optional.of(LAUNCHER_ALLAPPS_EXIT);
     }
 
-    private final Runnable mLauncherGcOpt = new Runnable() {
-        @Override
-        public void run() {
-            long currentTime = System.currentTimeMillis();
-            if (lastGcTime == 0L || currentTime - lastGcTime > GC_INTERVAL_MS) {
-                Log.v("GcOpt", "performing garbage collection for Launcher");
-                System.gc();
-                System.runFinalization();
-                System.gc();
-                lastGcTime = currentTime;
-            }
-        }
-    };
-
     @Override
     protected void onResume() {
         TraceHelper.INSTANCE.beginSection(ON_RESUME_EVT);
@@ -1350,8 +1333,6 @@ public class Launcher extends StatefulActivity<LauncherState>
         } else {
             mOverlayManager.onActivityResumed();
         }
-        
-        mHandler.removeCallbacks(mLauncherGcOpt);
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
@@ -1376,8 +1357,6 @@ public class Launcher extends StatefulActivity<LauncherState>
             mQuickSpace.onPause();
         }
         mAppWidgetHolder.setActivityResumed(false);
-        
-        mHandler.postDelayed(mLauncherGcOpt, 1000);
     }
 
     /**

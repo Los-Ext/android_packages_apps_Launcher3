251e0b73cbc26ad31b228e62854bf46b7f5208e8
ImSpiDy <SpiDy2713@gmail.com>
2025-06-05 21:49:55 +0000

    Revert "Launcher3: Use settings key for navbar inverse"
    This reverts commit 056f06762682d2151703312343747cbf2252c852.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -119,7 +119,7 @@ public class TaskbarManager {
             Settings.Secure.NAV_BAR_KIDS_MODE);
 
     public static final Uri NAV_BAR_INVERSE = Settings.Secure.getUriFor(
-            Settings.Secure.NAVBAR_INVERSE_LAYOUT);
+            "sysui_nav_bar_inverse");
 
     public static final Uri ENABLE_TASKBAR = LineageSettings.System.getUriFor(
             LineageSettings.System.ENABLE_TASKBAR);

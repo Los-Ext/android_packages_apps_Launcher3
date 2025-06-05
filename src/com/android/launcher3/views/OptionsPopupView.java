d17c99f6188061c54494cf599873ec3e768a87f7
ImSpiDy <SpiDy2713@gmail.com>
2025-06-05 21:31:54 +0000

    launcher3: crdroid -> android
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- src/com/android/launcher3/views/OptionsPopupView.java
+++ src/com/android/launcher3/views/OptionsPopupView.java
@@ -56,7 +56,7 @@ import com.android.launcher3.widget.picker.WidgetsFullSheet;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.android.Utils;
 
 /**
  * Popup shown on long pressing an empty space in launcher

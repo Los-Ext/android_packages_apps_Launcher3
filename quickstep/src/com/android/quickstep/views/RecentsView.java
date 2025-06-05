febdd94831530ebb6b706dc75ddf7d666ca0647f
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-05 15:46:20 +0800

    RecentsView: fix crash when spamming Lock/Unlock button
    issue: https://github.com/AxionAOSP/issue_tracker/issues/41
    
    test
    monkey test
    
    Change-Id: If72936344ac98e12696febbda9d7d8aad437cc7e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -4695,7 +4695,11 @@ public abstract class RecentsView<
     private void updateLockIcon(String pkg) {
         boolean isLocked = getLockedTasks().contains(pkg);
         if (mLockButtonView != null) {
-            mLockButtonView.setImageResource(isLocked ? R.drawable.recents_locked : R.drawable.recents_unlocked);
+            int resId = isLocked ? R.drawable.recents_locked : R.drawable.recents_unlocked;
+            Drawable icon = androidx.core.content.ContextCompat.getDrawable(getContext(), resId);
+            if (icon != null) {
+                mLockButtonView.setImageDrawable(icon);
+            }
         }
     }
 

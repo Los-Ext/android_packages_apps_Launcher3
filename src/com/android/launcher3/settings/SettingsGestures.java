89853c1d4ae2a5ea223c1929ae5cb12b66c43673
ImSpiDy <SpiDy2713@gmail.com>
2025-11-18 23:53:03 +0000

    Revert "DerpLauncher: Update gesture settings to manage dependent preferences"
    This reverts commit 87532d0530d348cea69b02f26be37bbaf7d3a646.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- src/com/android/launcher3/settings/SettingsGestures.java
+++ src/com/android/launcher3/settings/SettingsGestures.java
@@ -219,7 +219,6 @@ public class SettingsGestures extends CollapsingToolbarBaseActivity
                             getDevicePrefs(getActivity()).edit().putString(KEY_HOMESCREEN_DT_GESTURES, dtGestureValue).commit();
                             doubletabAction.setValue(dtGestureValue);
                             doubletabAction.setSummary(doubletabAction.getEntry());
-                            updateDoubleTapDependentPreferences(dtGestureValue);
                             Toast.makeText(getActivity(), R.string.restarting_launcher_changes, Toast.LENGTH_SHORT).show();
                             Utilities.restartLauncher(getActivity());
                             return true;
@@ -235,7 +234,6 @@ public class SettingsGestures extends CollapsingToolbarBaseActivity
                             getDevicePrefs(getActivity()).edit().putString(KEY_HOMESCREEN_SWIPE_DOWN_GESTURES, swipeDownGestureValue).commit();
                             swipeDownAction.setValue(swipeDownGestureValue);
                             swipeDownAction.setSummary(swipeDownAction.getEntry());
-                            updateSwipeDownDependentPreferences(swipeDownGestureValue);
                             Toast.makeText(getActivity(), R.string.restarting_launcher_changes, Toast.LENGTH_SHORT).show();
                             Utilities.restartLauncher(getActivity());
                             return true;
@@ -259,12 +257,6 @@ public class SettingsGestures extends CollapsingToolbarBaseActivity
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
             }
-            
-            // Initialize enabled state of dependent preferences
-            String dtGestureValue = getDevicePrefs(getActivity()).getString(KEY_HOMESCREEN_DT_GESTURES, "1");
-            String swipeDownGestureValue = getDevicePrefs(getActivity()).getString(KEY_HOMESCREEN_SWIPE_DOWN_GESTURES, "0");
-            updateDoubleTapDependentPreferences(dtGestureValue);
-            updateSwipeDownDependentPreferences(swipeDownGestureValue);
         }
 
         @Override
@@ -367,34 +359,6 @@ public class SettingsGestures extends CollapsingToolbarBaseActivity
                             .performAccessibilityAction(ACTION_ACCESSIBILITY_FOCUS, null);
                 }
             });
-        }
-
-        /**
-         * Updates enabled state of double tap gesture dependent preferences
-         */
-        private void updateDoubleTapDependentPreferences(String gestureValue) {
-            Preference hapticsPref = findPreference("pref_haptics_on_dt_gestures");
-            if (hapticsPref != null) {
-                boolean isGestureEnabled = !"0".equals(gestureValue);
-                hapticsPref.setEnabled(isGestureEnabled);
-            }
-        }
-
-        /**
-         * Updates enabled state of swipe down gesture dependent preferences
-         */
-        private void updateSwipeDownDependentPreferences(String gestureValue) {
-            Preference hapticsPref = findPreference("pref_haptics_on_swipe_down_gestures");
-            Preference sidePref = findPreference("pref_swipe_down_side");
-            
-            boolean isGestureEnabled = !"0".equals(gestureValue);
-            
-            if (hapticsPref != null) {
-                hapticsPref.setEnabled(isGestureEnabled);
-            }
-            if (sidePref != null) {
-                sidePref.setEnabled(isGestureEnabled);
-            }
         }
     }
 }

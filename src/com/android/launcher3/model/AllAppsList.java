607021247a37663bd3091a91a7326906b925d2ff
ImSpiDy <SpiDy2713@gmail.com>
2026-01-24 12:14:57 +0000

    Revert "Launcher3: Switch to AppLock API for protected and hidden apps"
    This reverts commit 1114286577c4f3a9e38a3769cdd10c680980f12e.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- src/com/android/launcher3/model/AllAppsList.java
+++ src/com/android/launcher3/model/AllAppsList.java
@@ -38,7 +38,7 @@ import com.android.launcher3.compat.AlphabeticIndexCompat;
 import com.android.launcher3.dagger.ApplicationContext;
 import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.icons.IconCache;
-import com.android.launcher3.lineage.trust.AppLockHelper;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.model.data.AppInfo;
 import com.android.launcher3.model.data.AppsListData;
 import com.android.launcher3.model.data.ItemInfo;
@@ -87,7 +87,7 @@ public class AllAppsList {
 
     private boolean mDataChanged = false;
     private Consumer<AppInfo> mRemoveListener = NO_OP_CONSUMER;
-    private AppLockHelper mAppLockHelper;
+    private TrustDatabaseHelper mTrustData;
 
     private AlphabeticIndexCompat mIndex;
 
@@ -111,7 +111,7 @@ public class AllAppsList {
         mIconCache = iconCache;
         mAppFilter = appFilter;
         mRepo = repositoryProvider;
-        mAppLockHelper = AppLockHelper.getInstance(context);
+        mTrustData = TrustDatabaseHelper.getInstance(context);
         mIndex = new AlphabeticIndexCompat(LocaleList.getDefault());
     }
 
@@ -163,7 +163,7 @@ public class AllAppsList {
     }
 
     public void add(AppInfo info, LauncherActivityInfo activityInfo, boolean loadIcon) {
-        if (mAppLockHelper != null && mAppLockHelper.isPackageHidden(info.getTargetPackage())) {
+        if (mTrustData != null && mTrustData.isPackageHidden(info.getTargetPackage())) {
             return;
         }
         if (!mAppFilter.shouldShowApp(info.componentName)) {

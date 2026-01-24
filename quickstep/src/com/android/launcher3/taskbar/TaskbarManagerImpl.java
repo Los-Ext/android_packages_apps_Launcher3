1b77fc5107388de60e3307dd5c81523a1c8a7ec6
ImSpiDy <SpiDy2713@gmail.com>
2026-01-24 12:19:45 +0000

    Revert "Launcher3: Allow changing the height of gesture navbar [2/3]"
    This reverts commit 874c6985f3c2e78543b8f9d56b25da38811b4c8c.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManagerImpl.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManagerImpl.java
@@ -186,9 +186,6 @@ public class TaskbarManagerImpl implements DisplayDecorationListener {
     public static final Uri GESTURE_NAVBAR_LENGTH_MODE = Settings.System.getUriFor(
             Settings.System.GESTURE_NAVBAR_LENGTH_MODE);
 
-    public static final Uri GESTURE_NAVBAR_HEIGHT_MODE = Settings.System.getUriFor(
-            Settings.System.GESTURE_NAVBAR_HEIGHT_MODE);
-
     private static final LooperExecutor TASKBAR_UI_THREAD =
             new LooperExecutor("TASKBAR_UI_THREAD", THREAD_PRIORITY_FOREGROUND);
 
@@ -531,8 +528,6 @@ public class TaskbarManagerImpl implements DisplayDecorationListener {
                 .register(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .register(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
-        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
-                .register(GESTURE_NAVBAR_HEIGHT_MODE, mOnTaskBarChangeListener);
         if (DesktopExperienceFlags.ENABLE_SYS_DECORS_CALLBACKS_VIA_WM.isTrue()
                 && DesktopExperienceFlags.ENABLE_DISPLAY_CONTENT_MODE_MANAGEMENT.isTrue()) {
             displaysWithDecorationsRepositoryCompat
@@ -1253,8 +1248,6 @@ public class TaskbarManagerImpl implements DisplayDecorationListener {
                 .unregister(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .unregister(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
-        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
-                .unregister(GESTURE_NAVBAR_HEIGHT_MODE, mOnTaskBarChangeListener);
         if (DesktopExperienceFlags.ENABLE_SYS_DECORS_CALLBACKS_VIA_WM.isTrue()
                 && DesktopExperienceFlags.ENABLE_DISPLAY_CONTENT_MODE_MANAGEMENT.isTrue()) {
             mDisplaysWithDecorationsRepositoryCompat.unregisterDisplayDecorationListener(this);

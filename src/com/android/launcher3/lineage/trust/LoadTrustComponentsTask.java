08afde8fe883170afb90bfcd90936828d68b7f83
ImSpiDy <SpiDy2713@gmail.com>
2026-01-24 12:13:42 +0000

    Revert "Launcher3: LoadTrustComponents: Limit app list to only show launchable applications"
    This reverts commit c16d9ba61c1839e9ee1bf4a80c946c9efbbd62a3.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
+++ src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
@@ -73,10 +73,8 @@ public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<Trust
             PackageInfo app = apps.get(i);
             try {
                 String pkgName = app.packageName;
-                if ((!app.applicationInfo.isSystemApp() || launchablePackages.contains(pkgName) ||
-                        whiteListedPackages.contains(pkgName)) &&
-                        mPackageManager.getLaunchIntentForPackage(pkgName) != null) {
-
+                if (!app.applicationInfo.isSystemApp() || launchablePackages.contains(pkgName) ||
+                        whiteListedPackages.contains(pkgName)) {
                     String label = mPackageManager.getApplicationLabel(
                             mPackageManager.getApplicationInfo(pkgName,
                                     PackageManager.GET_META_DATA)).toString();

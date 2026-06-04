5247152c51dab0304073531719d618498585af2d
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-04 18:23:01 +0000

    Launcher3: Debug overview logs
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -61,6 +61,7 @@ import java.util.Arrays;
 public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayout
         implements OnClickListener, Insettable, SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String TAG = "OverviewActionsView";
+    private static final boolean DEBUG = false;
     private final Rect mInsets = new Rect();
 
     /**
@@ -385,8 +386,10 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
      *                      pair.
      */
     public void updateForGroupedTask(boolean isGroupedTask, boolean canSaveAppPair) {
-        Log.d(TAG, "updateForGroupedTask() called with: isGroupedTask = [" + isGroupedTask
-                + "], canSaveAppPair = [" + canSaveAppPair + "]");
+        if (DEBUG) {
+            Log.d(TAG, "updateForGroupedTask() called with: isGroupedTask = [" + isGroupedTask
+                    + "], canSaveAppPair = [" + canSaveAppPair + "]");
+        }
         mIsGroupedTask = isGroupedTask;
         mCanSaveAppPair = canSaveAppPair;
         updateActionButtonsVisibility();
@@ -412,8 +415,10 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         boolean showGroupActions = mIsGroupedTask && mDp.getDeviceProperties().isLargeScreen()
                 && mCanSaveAppPair
                 && !getContext().getSystemService(ActivityManager.class).isLowRamDevice();
+        if (DEBUG) {
         Log.d(TAG, "updateActionButtonsVisibility() called: showSingleTaskActions = ["
                 + showSingleTaskActions + "], showGroupActions = [" + showGroupActions + "]");
+        }
         getActionsAlphas().get(INDEX_GROUPED_ALPHA).setValue(showSingleTaskActions ? 1 : 0);
         getGroupActionsAlphas().get(INDEX_GROUPED_ALPHA).setValue(showGroupActions ? 1 : 0);
     }

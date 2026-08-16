2fde5cf45344d9d93803a5272aa6a1aaf6f16847
ImSpiDy <SpiDy2713@gmail.com>
2026-08-16 21:32:51 +0000

    Revert "Launcher3: Fix cloned apps not appearing in app drawer"
    This reverts commit c8a71d1e3e989f481ddfee1c213e3fb060ca701d.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -44,7 +44,6 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.os.UserManager;
 import android.os.Process;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -136,7 +135,8 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     protected final Context mContext;
     protected final T mActivityContext;
     protected final List<AdapterHolder> mAH;
-    protected final Predicate<ItemInfo> mPersonalMatcher;
+    protected final Predicate<ItemInfo> mPersonalMatcher = ItemInfoMatcher.ofUser(
+            Process.myUserHandle());
     protected WorkProfileManager mWorkManager;
     protected final PrivateProfileManager mPrivateProfileManager;
     protected final Point mFastScrollerOffset = new Point();
@@ -206,8 +206,6 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
         mContext = context;
         mActivityContext = ActivityContext.lookupContext(context);
         mAllAppsStore = mActivityContext.getActivityComponent().getAppsStore();
-        UserManager userManager = mActivityContext.getSystemService(UserManager.class);
-        mPersonalMatcher = ItemInfoMatcher.ofCurrentOrDualUser(userManager, Process.myUserHandle());
 
         mHeaderThreshold = getResources().getDimensionPixelSize(
                 R.dimen.dynamic_grid_cell_border_spacing);
--- src/com/android/launcher3/util/ItemInfoMatcher.java
+++ src/com/android/launcher3/util/ItemInfoMatcher.java
@@ -18,7 +18,6 @@ package com.android.launcher3.util;
 
 import android.content.ComponentName;
 import android.os.UserHandle;
-import android.os.UserManager;
 
 import androidx.annotation.NonNull;
 
@@ -44,25 +43,6 @@ public abstract class ItemInfoMatcher {
 
     public static Predicate<ItemInfo> ofUser(UserHandle user) {
         return info -> info != null && info.user.equals(user);
-    }
-
-    public static Predicate<ItemInfo> ofCurrentOrDualUser(UserManager userManager, UserHandle user) {
-        return itemInfo -> itemInfo != null && (
-                isDualAppUser(userManager, itemInfo.user) || itemInfo.user.equals(user)
-        );
-    }
-
-    private static boolean isDualAppUser(UserManager userManager, UserHandle user) {
-        return user.getIdentifier() == getCloneUserId(userManager);
-    }
-
-    public static int getCloneUserId(UserManager userManager) {
-        for (UserHandle userHandle : userManager.getUserProfiles()) {
-            if (userManager.getUserInfo(userHandle.getIdentifier()).isCloneProfile()) {
-                return userHandle.getIdentifier();
-            }
-        }
-        return -1;
     }
 
     public static Predicate<ItemInfo> ofComponents(
